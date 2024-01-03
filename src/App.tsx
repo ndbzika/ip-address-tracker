e1269adfdd89b72e2899fc02adcf678feb411073
ndbzika <chakcpps@gmail.com>
2024-01-03 12:26:48 -0300

    feat: Title & Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import {FaChevronRight} from 'react-icons/fa'
+import { Container, InputContainer } from './style'
 
 function App() {
   return (
-    <div>
+    <Container>
       <h1>IP Address Tracker</h1>
 
-      <div>
-      <input type="text" />
-      <button><FaChevronRight/></button>
-      </div>
-    </div>
+      <InputContainer>
+        <input type="text" placeholder='Search for any IP address or domain'/>
+        <button><FaChevronRight style={{color: '#fff', width: '1rem', height: '1rem', textAlign: 'center'}}/></button>
+      </InputContainer>
+
+      {/* component de dados de ip */}
+
+      {/* mapa */}
+    </Container>
   )
 }
 
---
+++ src/style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: url("https://efim-frontendmentor-ip-tracker.pages.dev/images/pattern-bg-desktop.png");
+    background-repeat: no-repeat;
+    background-size: 100% 50%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        color: #ffffff;
+        margin: 2rem 0;
+    }
+`;
+
+export const InputContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    input {
+        width: 28rem;
+        height: 3.5rem;
+        padding: 1.1rem;
+        border-radius: 15px 0 0 15px;
+        font-size: 18px;
+
+        &::placeholder {
+            font-size: 18px;
+            text-align: left;
+            color: hsl(0, 0%, 59%);
+        }
+    }
+
+    button {
+        width: 3.5rem;
+        height: 3.5rem;
+        border-radius: 0 1rem 1rem 0;
+        background-color: #000000;
+    }
+`;
