370a7edd924e0cc9e95ba90cb067a41115ea9cc8
ndbzika <chakcpps@gmail.com>
2024-01-03 11:53:25 -0300

    :tada: init: Configurando projeto

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {FaChevronRight} from 'react-icons/fa'
+
+function App() {
+  return (
+    <div>
+      <h1>IP Address Tracker</h1>
+
+      <div>
+      <input type="text" />
+      <button><FaChevronRight/></button>
+      </div>
+    </div>
+  )
+}
+
+export default App
